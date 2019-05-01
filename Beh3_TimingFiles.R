873e4148934df73d93b2e8248c9be082065f14e1
nmuncy <pingamormn@gmail.com>
2019-05-01 09:43:50 -0600

    a touch of work on beh3

--- Beh3_TimingFiles.R
+++ Beh3_TimingFiles.R
@@ -7,3 +7,17 @@ inputDir <- "/Volumes/Vedder/FoodMST/Analyses/behAnalysis/all_data/converted/"
 outputDir <- "/Volumes/Vedder/FoodMST/Analyses/behAnalysis/timing_files/"
 
 txtList <- list.files(path = inputDir, pattern = ".*.txt")
+txtList <- t(txtList)
+
+for(j in txtList){
+  
+  data_raw <- read.delim(paste0(inputDir,j),sep=" ")
+  
+  # blocks
+  ind_blockList <- grep("BlockList:",data_raw[,1])
+  num_blocks <- as.numeric(length(ind_blockList))
+  
+  ind_stimImage <- grep("StimImage:", data_raw[,1])
+  ind_respCorr <- grep("Correct:", data_raw[,1])
+  ind_resp <- grep("Stim.RESP:", data_raw[,1])
+}

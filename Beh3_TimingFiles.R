b4becb63e5cc43796a12b7c36148723321b2bf7b
nmuncy <pingamormn@gmail.com>
2019-05-02 09:40:00 -0600

    update timing file

--- Beh3_TimingFiles.R
+++ Beh3_TimingFiles.R
@@ -9,6 +9,7 @@ outputDir <- "/Volumes/Vedder/FoodMST/Analyses/behAnalysis/timing_files/"
 txtList <- list.files(path = inputDir, pattern = ".*.txt")
 txtList <- t(txtList)
 
+# j <- txtList[1]
 for(j in txtList){
   
   data_raw <- read.delim(paste0(inputDir,j),sep=" ")
@@ -28,6 +29,7 @@ for(j in txtList){
   ind_corr <- grep("Correct:", data_raw[,1])
   ind_resp <- grep("Stim.RESP:", data_raw[,1])
   
+  
   # correct for multiple responses - assumes no double response on first trial
   #### does this work?
   cor_range <- as.numeric(ind_stimImage[2]-ind_stimImage[1])
@@ -45,14 +47,59 @@ for(j in txtList){
     break
   }
   
-  # extract useful info
-  hold_stimImage <- as.character(data_raw[ind_stimImage,2])
-  hold_stimType <- as.numeric(as.character(data_raw[ind_stimType,2]))
-  hold_corr <- as.numeric(as.character(data_raw[ind_corr,2]))
-  hold_resp <- as.numeric(as.character(data_raw[ind_resp,2]))
   
-  # # determine behavior
-  # for(i in 1:length(hold_stimImage)){
+  # split into blocks
+  var_list <- c("stimImage","stimType","corr","resp")
+  
+  for (i in 1:length(ind_blockList)) {
+    for (k in var_list) {
+      assign(paste0("ind_",k,"_block",i),NA)
+    }
+  }
+
+  for(i in 1:length(ind_stimImage)){
+   for(k in 1:length(ind_blockList)){
+     if(as.numeric(ind_stimImage[i]) < ind_blockList[k]){
+       for(m in var_list){
+         assign(paste0("ind_",m,"_block",k),c(get(paste0("ind_",m,"_block",k)),as.numeric(get(paste0("ind_",m))[i])))
+       }
+       break
+     }
+   } 
+  }
+  
+  for (i in 1:length(ind_blockList)) {
+    for (k in var_list) {
+      assign(paste0("ind_",k,"_block",i),get(paste0("ind_",k,"_block",i))[-1])
+    }
+  }
+  
+  
+  # extract useful info
+  for(i in 1:length(ind_blockList)){
+    for(k in var_list){
+      if(k != "stimImage"){
+        assign(paste0("hold_",k,"_block",i),as.numeric(as.character(data_raw[get(paste0("ind_",k,"_block",i)),2])))
+      }else{
+        assign(paste0("hold_",k,"_block",i),as.character(data_raw[get(paste0("ind_",k,"_block",i)),2]))
+      }
+    }
+  }
+
+  
+  ## determine behavior - split for stim types
+  # (TH = 61, TL = 62, TO = 63, LH = 71, LL = 72, OL = 73, FH = 91, FL = 92, FO = 93)
+  # (1 = old, 2 = sim, 3 = new)
+  # for(i in 1:length(ind_blockList)){
   #   
   # }
 }
+
+
+
+
+
+
+
+
+
